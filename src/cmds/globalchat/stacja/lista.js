fc1637359672d304e6b8ea0b073662d2682532f5
Patyczakus <75316297+Patyczakus@users.noreply.github.com>
2024-06-16 17:54:44 +0200

    Update lista.js

--- src/cmds/globalchat/stacja/lista.js
+++ src/cmds/globalchat/stacja/lista.js
@@ -9,13 +9,13 @@ module.exports = {
      */
     async execute(client, interaction) {
         await interaction.deferReply()
-        var stations = Object.entries(db.get("stations"))
+        var stations = Object.entries(db.get("stations").val ?? {})
             .sort(() => Math.random() - 0.5)
             .filter((x, i) => i < 15)
         const embed = new EmbedBuilder()
             .setTitle("Lista stacji w losowej kolejności")
             .setDescription(
-                `Wyświetlane jest maks. 15\n\n${stations.map((x) => `\`${x[0]}\` (<@${x[1].split("|")[0]}>, ${x[1].split("|")[1] ? "Na hasło" : "Publiczny"})`).join("\n")}`
+                `Wyświetlane jest maks. 15 stacji\n\n${stations.map((x) => `\`${x[0]}\` (<@${x[1].split("|")[0]}>, ${x[1].split("|")[1] ? "Na hasło" : "Publiczny"})`).join("\n")}`
             )
             .setColor("Random")
 
