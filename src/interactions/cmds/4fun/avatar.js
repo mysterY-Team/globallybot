c894e832a96cd8c22cae8882b838710257fdff8c
vxhtyy <81436887+vxhtyy@users.noreply.github.com>
2024-07-22 01:02:14 +0200

    Update avatar.js

--- src/interactions/cmds/4fun/avatar.js
+++ src/interactions/cmds/4fun/avatar.js
@@ -1,1 +1,22 @@
+const { CommandInteraction, Client, EmbedBuilder } = require('discord.js');
 
+module.exports = {  
+
+        /**
+         *
+         * @param {Client} client
+         * @param {CommandInteraction} interaction
+        */
+       async execute (interaction, client) {
+                const user = interaction.options.getUser("user", false)|| interaction.user;
+                const avatarUrl = user.displayAvatarURL({ dynamic: true, size: 2048 });
+                const avatarEmbed = new EmbedBuilder()
+                .setTitle("Avatar")
+                .setDescription(`[Download](${avatarUrl})`) 
+                .setColor("#58D68D") 
+                .setImage(avatarUrl)
+                .setFooter({ text: `Globally, powered by "mysterY Devs" team`,  iconURL: "https://cdn.discordapp.com/avatars/1228622088047431772/201499823222ecca6482c9e71cac13e6.webp" });
+                await interaction.reply({ embeds: [avatarEmbed] })
+            
+        },
+};
