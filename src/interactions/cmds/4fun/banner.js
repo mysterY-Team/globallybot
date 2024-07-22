fc60aac07ea44e16c8531b178478f632170a5283
Patyczakus <75316297+Patyczakus@users.noreply.github.com>
2024-07-22 15:15:21 +0200

    Upiększenie i poprawa

--- src/interactions/cmds/4fun/banner.js
+++ src/interactions/cmds/4fun/banner.js
@@ -1,27 +1,26 @@
-const { CommandInteraction, EmbedBuilder } = require('discord.js');
+const { CommandInteraction, EmbedBuilder, Client } = require("discord.js")
 
-module.exports = {  
+module.exports = {
     /**
+     * @param {Client} client
      * @param {CommandInteraction} interaction
-    */
-    async execute(interaction) {
-        const user = interaction.options.getUser("user", false) || interaction.user;
+     */
+    async execute(client, interaction) {
+        const user = interaction.options.getUser("user", false) || interaction.user
 
-        const fetchedUser = await interaction.client.users.fetch(user.id, { force: true });
-        const bannerUrl = fetchedUser.bannerURL({ dynamic: true, size: 512});
+        const fetchedUser = await interaction.client.users.fetch(user.id, { force: true })
+        const bannerUrl = fetchedUser.bannerURL({ dynamic: true, size: 512 })
 
         if (!bannerUrl) {
-            await interaction.reply({ content: "Ten użytkownik nie posiada banneru", ephemeral: true });
-            return;
+            await interaction.reply({ content: "Ten użytkownik nie posiada banneru", ephemeral: true })
+            return
         }
 
-        const bannerEmbed = new EmbedBuilder()
-            .setTitle("Banner")
-            .setDescription(`[Download](${bannerUrl})`)
-            .setColor("#58D68D")
-            .setImage(bannerUrl)
-            .setFooter({ text: `Globally, powered by "mysterY Devs" team`, iconURL: "https://cdn.discordapp.com/avatars/1228622088047431772/201499823222ecca6482c9e71cac13e6.webp" });
+        const bannerEmbed = new EmbedBuilder().setTitle("Banner").setDescription(`[Download](${bannerUrl})`).setColor("#58D68D").setImage(bannerUrl).setFooter({
+            text: `Globally, powered by "mysterY Devs" team`,
+            iconURL: "https://cdn.discordapp.com/avatars/1228622088047431772/201499823222ecca6482c9e71cac13e6.webp",
+        })
 
-        await interaction.reply({ embeds: [bannerEmbed] });
+        await interaction.reply({ embeds: [bannerEmbed] })
     },
-};
+}
