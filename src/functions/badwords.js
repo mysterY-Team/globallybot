1b7184e48062e381328f402323cde7647ddf0402
Patyczakus <75316297+Patyczakus@users.noreply.github.com>
2024-08-30 23:23:08 +0200

    Jeszcze lepszy system zakazanych słów

--- src/functions/badwords.js
+++ src/functions/badwords.js
@@ -84,7 +84,7 @@ const bannedWords = [
 
 function checkAnyBadWords(text) {
     for (let i = 0; i < bannedWords.length; i++) {
-        const regex = new RegExp(`([^a-z0-9])${bannedWords[i]}([^a-z0-9])|^${bannedWords[i]}$`, "i")
+        const regex = new RegExp(`([^a-z0-9])${bannedWords[i]}([^a-z0-9])|^(${bannedWords[i]}(?: *))+`, "i")
         if (regex.test(text)) {
             return { checked: true, badWord: bannedWords[i] }
         }
