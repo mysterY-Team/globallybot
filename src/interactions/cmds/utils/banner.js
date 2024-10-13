b3751bd3d37018d190842bb8535f53900a5bccf6
vxhtyy <81436887+vxhtyy@users.noreply.github.com>
2024-10-13 23:30:12 +0200

    Update banner.js

--- src/interactions/cmds/utils/banner.js
+++ src/interactions/cmds/utils/banner.js
@@ -18,9 +18,9 @@ module.exports = {
         const bannerEmbed = new EmbedBuilder().setTitle("Banner").setColor("Random").setImage(bannerUrl).setFooter({
             text: `Globally, powered by mysterY`,
         })
-        const btns = new ActionRowBuilder().addComponents(new ButtonBuilder().setStyle(ButtonStyle.Link).setURL(bannerUrl).setLabel("Link do banneru"))
+        // const btns = new ActionRowBuilder().addComponents(new ButtonBuilder().setStyle(ButtonStyle.Link).setURL(bannerUrl).setLabel("Link do banneru"))
 
-        await interaction.reply({ embeds: [bannerEmbed], components: [btns] })
+        await interaction.reply({ embeds: [bannerEmbed] })
     },
 }
 a
